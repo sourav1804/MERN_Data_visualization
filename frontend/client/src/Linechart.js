a3c36cb8d057823656de8053c449560eadc0302a
sourav1804 <71638004+sourav1804@users.noreply.github.com>
2024-04-19 20:42:28 +0530

    Update Linechart.js

--- frontend/client/src/Linechart.js
+++ frontend/client/src/Linechart.js
@@ -24,7 +24,7 @@ function LinegraphData() {
     useEffect(() => {
         const fetchOrders = async () => {
             try {
-                const response = await Axios.get('http://localhost:3300/getorders');
+                const response = await Axios.get('https://mern-data-visualization.vercel.app/getorders');
                 setOrders(response.data);
                 setLoading(false);
             } catch (error) {
